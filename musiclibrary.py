9f657bc17dcb711563b00973eb7f5caf3f6e0ae7
ali-hassan176 <m.ali.hassan191@gmail.com>
2025-06-25 11:25:43 +0500

    add instruction

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,5 +1,6 @@
 music={
     "jhol":"https://youtu.be/BWczaSneA0Q",
     "skyfall":"https://youtu.be/sZrTJesvJeo",
-    "palpal":"https://youtu.be/8of5w7RgcTc"
+    "palpal":"https://youtu.be/8of5w7RgcTc",
+    "secrets":"https://youtu.be/PzU2ru1kcJw?list=RDMM"
 }
